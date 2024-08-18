cad96a243aa3d3b556a904adbf5a73fbd6267ea4
Ahmad Nazmuddin <anazmuddin072@gmail.com>
2024-08-18 22:04:03 +0700

    baru header

---
+++ script.js
@@ -0,0 +1,9 @@
+const sideMenu = document.querySelector('#sideMenu');
+
+function openMenu(){
+    sideMenu.style.transform = 'translateX(-16rem)';
+}
+
+function closeMenu(){
+    sideMenu.style.transform = 'translateX(16rem)';
+}

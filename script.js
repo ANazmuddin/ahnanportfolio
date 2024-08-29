2f6b0ed6477d2e0107bfc3fc2eb82a96dbccdd6b
Ahmad Nazmuddin <anazmuddin072@gmail.com>
2024-08-29 17:37:07 +0700

    animated

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const sideMenu = document.querySelector('#sideMenu');
+const navBar = document.querySelector("nav");
+const navLinks = document.querySelector("nav ul");
 
 function openMenu(){
     sideMenu.style.transform = 'translateX(-16rem)';
@@ -7,3 +9,13 @@ function openMenu(){
 function closeMenu(){
     sideMenu.style.transform = 'translateX(16rem)';
 }
+
+window.addEventListener('scroll', ()=>{
+    if(scrollY > 50){
+        navBar.classList.add('bg-white', 'bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm');
+        navLinks.classList.remove('bg-white', 'shadow-sm', 'bg-opacity-50');
+    }else{
+        navBar.classList.remove('bg-white', 'bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm');
+        navLinks.classList.add('bg-white', 'shadow-sm', 'bg-opacity-50');
+    }
+})
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+tailwind.config = {
+    theme:{
+        extend:{
+            gridTemplateColums:{
+                'auto': 'repeat(auto-fit, minmax(200px, 1fr))'
+            },
+            fontFamily:{
+                Outfit: ["Outfit", "sans-serif"],
+                Ovo: ["Ovo", "serif"]
+            },
+            animation:{
+                spin_slow: 'spin 6s linear infinite'
+            },
+            colors:{
+                lightHover: '#fcf4ff',
+                darkHover: '#2a004a',
+            },
+            boxShadow:{
+                'black': '4px_4px_0_#000',
+                'white': '4px_4px_0_#fff',
+            }
+        }
+    }
+}
